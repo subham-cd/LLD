0fd0d0af103c83cdabe168f4e12e1cf72a45270a
Subham Singh <>
2026-02-10 13:36:37 +0530

    Lesson 1 code

---
+++ src/Lesson1/Employee.java
@@ -0,0 +1,22 @@
+package Lesson1;
+///issue in code ?
+public class Employee {
+    private int id;
+    public int getId(){
+        return this.id;
+    }
+    public String fetchBioData(){
+        return "Some bio Data";
+    }
+    public double calculateSalary(){
+        return 0;
+    }
+    public void printPerformanceData(){
+        System.out.println("Some Performance related data");
+    }
+}
+
+
+//here same problem in code that SRP->single responsibility Principle
+
+//it make resusablity difficult and class method heavy
--- src/Lesson1/ExampleFrontend/ProblematicCode/UserProfile.js
+++ src/Lesson1/ExampleFrontend/ProblematicCode/UserProfile.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 // Is there any issue with this code ????
 function UserProfile() {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);  //loading should be true
     const [error, setError] = useState('');
 
+
+//umount problem
+
+///we should use isMounted =true
     useEffect(() => {
         setLoading(true);
-        axios.get('/api/user/123')
+        axios.get('/api/user/123')///hard coded api problem
             .then(response => setUser(response.data))
             .catch(error => setError('Error fetching user data'))
             .finally(() => setLoading(false));
